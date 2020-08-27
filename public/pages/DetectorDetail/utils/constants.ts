5bc9077cf7a5ebfc7909589540ca0afcab0e613e
Yizhe Liu <59710443+yizheliu-amazon@users.noreply.github.com>
2020-08-26 17:47:37 -0700

    Add action item and message for init failue case due to invalid search query.  (#285)
    * Add action item and message for init failue case due to invalid search query. Issue:#218

--- public/pages/DetectorDetail/utils/constants.ts
+++ public/pages/DetectorDetail/utils/constants.ts
@@ -67,6 +67,14 @@ export const DETECTOR_INIT_FAILURES = Object.freeze({
     cause: 'the detector is not defined',
     actionItem: 'Define your detector and try again.',
   },
+  INVALID_FEATURE_QUERY: {
+    // TODO: https://github.com/opendistro-for-elasticsearch/anomaly-detection/issues/218
+    //https://github.com/opendistro-for-elasticsearch/anomaly-detection/blob/master/src/main/java/com/amazon/opendistroforelasticsearch/ad/constant/CommonErrorMessages.java#L28
+    keyword: 'Invalid search query',
+    cause: 'the custom query caused an invalid feature aggregation',
+    actionItem:
+      'Revise the custom query in your feature aggregations and try again.',
+  },
   UNKNOWN_EXCEPTION: {
     //https://github.com/opendistro-for-elasticsearch/anomaly-detection/blob/development/src/main/java/com/amazon/opendistroforelasticsearch/ad/transport/AnomalyResultTransportAction.java#L438
     keyword: 'We might have bugs',
